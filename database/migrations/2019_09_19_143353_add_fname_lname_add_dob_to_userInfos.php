42764604b90de173cbbd3aedb9cd6b63f8017a8f
Hardik Chavda <hardikkchavda@gmail.com>
2019-09-25 18:20:24 +0530

    pull

---
+++ database/migrations/2019_09_19_143353_add_fname_lname_add_dob_to_userInfos.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFnameLnameAddDobToUserInfos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_infos', function (Blueprint $table) {
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('address');
+            $table->string('dob');
+        });        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('userInfos', function (Blueprint $table) {
+           
+        });
+    }
+}
